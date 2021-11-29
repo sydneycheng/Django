a2191708465276ca26bc726d997bb57dd47dfdfd
Sydney Cheng <sydney_cheng1@baylor.edu>
2021-11-29 17:05:36 -0600

    topic and topics html

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 app_name = "MainApp"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("topics", views.topics, name="topics"),
+    # we've created an integer variable to distinguish via a PK for chess, rock climbing, etc
+    # this acts as an identifying value
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
+]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 
+from .models import Topic
+
+
 # Create your views here.
 def index(request):
     return render(request, "MainApp/index.html")
+
+
+def topics(request):
+    topics = Topic.objects.order_by(
+        "date_added"
+    )  # if you put a '-' in front of "date_added" it would sort by descending order
+
+    context = {"topics": topics}
+    # context is our dictionary
+    # the key is the variable used in the template (html) file;
+    # the value of the dictionary is the variable used in the view function
+
+    return render(request, "MainApp/topics.html", context)
+    # we are passing this dictionary (context) to our html file
+
+
+# topic_id comes from urls.py
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    entries = topic.entry_set.all()
+
+    # key represents the variable name in the template
+    # value represents the varible name in the view
+    context = {"topic": topic, "entries": entries}
+
+    return render(request, "MainApp/topic.html", context)
