6ac74d4e891c743ce6a8b83beba0273ce7867009
Sydney Cheng <sydney_cheng1@baylor.edu>
2021-12-06 16:51:05 -0600

    create register.html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method != "POST":
+        fom = (
+            UserCreationForm()
+        )  # we are loading up a blank form for this GET request (reading from database)
+    else:
+        form = UserCreationForm(
+            data=request.POST
+        )  # if the method is POST (writing to databse)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("MainApp:index")
+
+    context = {"form": form}
+    return render(request, "registration/register.html;, context")
